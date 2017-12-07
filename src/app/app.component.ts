75e36c80a443fa5a2d317d3417b2b43725316a43
Hugo Des <hugo.deslongchamps@gmail.com>
2017-12-07 10:27:16 +0100

    Demo error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IMyOptions} from 'mydatepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  myDatePickerOptions: IMyOptions = {
+    // other options...
+    dateFormat: 'dd-mm-yyyy',
+  };
+  public date: any;
+
+  ngOnInit() {
+    const d = new Date();
+    this.date = { date: {year: d.getFullYear(), month: d.getMonth() + 1, day: d.getDate() } };
+    setInterval(() => {
+      console.log(this.date);
+    }, 1000);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MyDatePickerModule} from 'mydatepicker';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyDatePickerModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
